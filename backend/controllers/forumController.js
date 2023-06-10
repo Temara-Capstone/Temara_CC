6d3bfd225181b2c669658ec6c47da8a17bf4451a
revaldyazura <91121018+revaldyazura@users.noreply.github.com>
2023-06-11 00:28:44 +0700

    Update forumController.js
    controller for forum-api

--- backend/controllers/forumController.js
+++ backend/controllers/forumController.js
@@ -1,28 +1,126 @@
-const Forum = require('../models/ForumModel')
+const forum = require('../models/forumModel')
+// const conn = require('../db/db')
 
-const postThread = (req,res)=>{
-    try{
-        Forum.postThreadUser(req, (err, result)=>{
-            if(err){
-                console.error(err)
+const getThread = (req, res) => {
+    forum.getAllThread((err, thread) => {
+        if (err) throw err
+        res.json(thread)
+    }, res)
+}
+
+const getByThreadID = (req, res) => {
+
+    forum.getThreadByID(req, (err, fields) => {
+        if (err) {
+            console.error(err)
+            return res.status(404).json({
+                status: 'error',
+                message: 'Thread Not Found'
+            })
+        }
+        if (fields.length === 0) {
+            return res.status(400).json({
+                status: 'error',
+                message: 'Thread Not Found'
+            })
+        }
+        res.status(200).json({
+            status: 'success',
+            message: 'Thread Found',
+            result: fields
+        })
+    })
+
+}
+
+const getByUserID = (req, res) => {
+    forum.getThreadByUser(req, (err, fields) => {
+        if (err) {
+            console.error(err)
+            return res.status(404).json({
+                status: 'error',
+                message: 'User Not Found'
+            })
+        }
+        if (fields.length === 0) {
+            return res.status(400).json({
+                status: 'error',
+                message: 'User Not Found'
+            })
+        }
+        res.status(200).json({
+            status: 'success',
+            message: 'User Found',
+            result: fields
+        })
+    })
+}
+
+const postingThread = async (req, res) => {
+    try {
+        forum.postThread(req, (err) => {
+            if (err) {
                 res.status(401).json({
                     status: 'error',
-                    message: 'Data Invalid'
+                    message: 'Data invalid'
+                })
+            } else {
+                res.status(200).json({
+                    status: 'success',
+                    message: 'Data added'
                 })
             }
-            res.status(200).json({
-                status: 'success',
-                message: 'Data Thread Successfully Created',
-                result: result
-            })
         })
-    }catch (e) {
+    } catch (e) {
         console.error(e)
-        res.status(500).json({
-            status: 'error',
-            message: 'Internal Server Error'
-        })
+        res.status(500).json({ status: 'error', message: "Internal Server Error" })
     }
 }
 
-module.exports = { postThread }
+const updatingThread = async (req, res) => {
+    try {
+        forum.updateThread(req, (err) => {
+            if (err) {
+                res.status(401).json({
+                    status: 'error',
+                    message: 'Data invalid'
+                })
+            } else {
+                res.status(200).json({
+                    status: 'success',
+                    message: 'Data updated'
+                })
+            }
+        })
+    } catch (e) {
+        console.error(e)
+        res.status(500).json({ status: 'error', message: "Internal Server Error" })
+    }
+}
+
+const delThread = (req, res) => {
+    forum.deleteThread(req, (err) => {
+        if (err) {
+            console.error(err)
+        } else {
+            res.status(201).send("Data deleted")
+        }
+    })
+}
+
+
+// const response = (statusCode, data, message, res) => {
+//     res.json(statusCode, [
+//         {
+//             payload: data,
+//             message,
+//             metadata: {
+//                 prev: "",
+//                 next: "",
+//                 current: "",
+//             },
+//         },
+//     ])
+// }
+
+module.exports = { getByThreadID, getByUserID, getThread, postingThread, updatingThread, delThread }
