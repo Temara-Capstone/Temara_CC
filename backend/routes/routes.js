e829eb5eeda31e2c8d4881274f11521e934f1497
revaldyazura <91121018+revaldyazura@users.noreply.github.com>
2023-06-11 00:30:27 +0700

    Update routes.js
    add new routes and variable

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -7,6 +7,9 @@ const {
     profile,
     getUserById
 }  = require('../controllers/userData')
+const {
+    getByThreadID, getByUserID, getThread, postingThread, updatingThread, delThread
+}  = require('../controller/forumController')
 
 const VerifyToken = require('../middleware/VerifyToken')
 const refreshToken = require('../middleware/RefreshToken')
@@ -28,11 +31,16 @@ const setupRoutes = (app)=>{
     app.get('/api/token', refreshToken)
     app.delete('/api/logout', logout)
     app.get('/api/users/profile/:id', profile)
-    app.post('/api/forum', postThread)
     app.get('/api/users/user/:id', getUserById)
     app.get('/api/articles', getAllArticles)
     app.get('/api/articles/:id', getArticleById)
     app.get('/api/quotes', getRandomQuotes)
+    app.get('/api/forum', getThread)
+    app.get('/api/forum/:id', getByThreadID)
+    app.get('/api/forum/user/:user_id', getByUserID)
+    app.post('/api/forum', postingThread)
+    app.put('/api/forum/:id', updatingThread)
+    app.delete('/api/forum/:id', delThread)
 }
 
 module.exports = setupRoutes
